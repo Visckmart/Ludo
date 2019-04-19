3791219e3fcc6b1c1002abc0d77594b06d9b6a58
Visckmart <visckmart@hotmail.com>
2019-04-19 17:07:43 -0300

    Initial Commit

---
+++ Ludo/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Ludo
+//
+//  Created by Victor Martins on 19/04/19.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
