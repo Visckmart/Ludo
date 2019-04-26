e06b5a55d7967c7bf9d9207ba16882bc7cc5848e
Thlamz <thlamenza@hotmail.com>
2019-04-25 22:25:08 -0300

    Assertivas adicionadas no modulo circular

--- Ludo/Circular.c
+++ Ludo/Circular.c
@@ -3,6 +3,7 @@
 //  Ludo
 //
 #include <stdlib.h>
+#include <assert.h>
 #include "Circular.h"
 
 /**************************************
@@ -18,8 +19,14 @@ struct circular{
 
 CIR_lstCircular *CIR_CriaLista(void *pCont) //Cria uma nova lista composta de um elemento que aponta para sí mesmo
 {
-	CIR_lstCircular *circ = (CIR_lstCircular*) malloc(sizeof(CIR_lstCircular));
-	if(circ==NULL) return NULL;
+	CIR_lstCircular *circ;
+	circ = (CIR_lstCircular*) malloc(sizeof(CIR_lstCircular));
+	
+	#ifdef _DEBUG
+	assert(circ!=NULL && pCont!=NULL);
+	#endif
+	
+	
 	circ ->proximo = circ;
 	circ ->anterior = circ;
 	circ ->pCont = pCont;
@@ -38,9 +45,11 @@ void CIR_DestroiLista(CIR_lstCircular *pCircular)
 CIR_lstCircular *CIR_InsereElemento(CIR_lstCircular *pLista,void *pCont)
 {
 	CIR_lstCircular *novo,*prox = CIR_ProximoElemento(pLista);
+	novo = (CIR_lstCircular*) malloc(sizeof(CIR_lstCircular));//Maloca e insere um elemento ajustando os ponteiros
 	
-	novo = (CIR_lstCircular*) malloc(sizeof(CIR_lstCircular));//Maloca e insere um elemento ajustando os 
-	if(novo==NULL) return NULL;
+	#ifdef _DEBUG
+	assert(novo!=NULL && pCont!=NULL););
+	#endif
 	
 	novo->pCont = pCont;
 	novo->anterior = pLista;
@@ -48,6 +57,9 @@ CIR_lstCircular *CIR_InsereElemento(CIR_lstCircular *pLista,void *pCont)
 	pLista->proximo = novo;
 	prox->anterior = novo;
 	
+	#ifdef _DEBUG
+	assert(novo->anterior->proximo == novo && novo->proximo->anterior == novo);
+	#endif
 	return novo;
 }
 
@@ -56,32 +68,52 @@ CIR_lstCircular *CIR_RemoveElemento(CIR_lstCircular *pLista)
 	CIR_lstCircular *ret = CIR_PrecedenteElemento(pLista);
 	if(ret == pLista) ret = NULL;
 	
+	#ifdef _DEBUG
+	assert(ret!=NULL && pLista!=NULL);
+	#endif
+	
 	pLista->anterior->proximo = pLista->proximo;
 	pLista->proximo->anterior = pLista->anterior;
 	free(pLista);
+	
 	return ret;
 }
 CIR_lstCircular *CIR_ProximoElemento(CIR_lstCircular *pLista)
 {
+	#ifdef _DEBUG
+	assert(pLista!=NULL);
+	#endif
+	
 	return pLista->proximo;
 }
 CIR_lstCircular *CIR_PrecedenteElemento(CIR_lstCircular *pLista)
 {
+	#ifdef _DEBUG
+	assert(pLista!=NULL);
+	#endif
+	
 	return pLista->anterior;
 }
 void *CIR_Conteudo(CIR_lstCircular *pLista)
 {
+	#ifdef _DEBUG
+	assert(pLista!=NULL);
+	#endif
+	
 	return pLista->pCont;
 }
 
 CIR_lstCircular *CIR_ProcuraElemento(CIR_lstCircular *pLista,void *pCont)
 {
 	CIR_lstCircular *corrente = pLista->proximo;
+	#ifdef _DEBUG
+	assert(pLista!=NULL && pCont!=NULL&& corrente!=NULL);
+	#endif
 	
 	while(corrente!=pLista) //Marca o primeiro elemento e avança na lista circular ate encontrar ele novamente.
 	{
 		if(corrente->pCont==pCont) return corrente; //Se encontrar no caminho para e retorna o endereço.
 		corrente = corrente->proximo;
 	}
-	return NULL; //Fazer um tipo enumerado com erros?
+	return NULL;
 }
