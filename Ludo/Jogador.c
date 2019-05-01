8e576bfb097ac072857f4647812b37c3b29457a9
Visckmart <visckmart@hotmail.com>
2019-05-01 12:59:00 -0300

    Implementação da função que cria um jogador.

--- Ludo/Jogador.c
+++ Ludo/Jogador.c
@@ -6,19 +6,31 @@
 #include "Jogador.h"
 #include <stdlib.h>
 #include "Casa.h"
+#include "Circular.h"
 
 struct Jogador {
-    Casa * pecas;
+    CIR_lstCircular * pecas;
     Cor cor;
 };
 
 Jogador * JOG_Cria(Cor corDasPecas) {
+    // Aloca um espaço para o jogador
     Jogador * j = (Jogador *)malloc(sizeof(Jogador));
-    j->pecas = (Casa *)malloc(sizeof(int) * 4);
-    for (int i = 0; i < 4; i++) {
-        j->pecas[i] = CAS_Cria();
+    
+    // Cria a primeira peça
+    Casa * c = CAS_Cria();
+    j->pecas = CIR_CriaLista(c);
+    // Cria as peças adicionais
+    for (int i = 1; i < 4; i++) {
+        Casa * c = CAS_Cria();
+        CIR_lstCircular * l = CIR_CriaLista(c);
+        CIR_InsereElemento(l, j->pecas);
     }
+    
+    // Guarda a cor do jogador
     j->cor = corDasPecas;
+    
+    // Retorna o jogador criado
     return j;
 }
 
