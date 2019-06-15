f971bcd3327cefda68e243cd28a4ed3f0cac9e22
Visckmart <visckmart@hotmail.com>
2019-06-15 16:02:27 -0300

    Função para obter o número de jogadores

--- Ludo/Partida.c
+++ Ludo/Partida.c
@@ -35,9 +35,10 @@ static int PAR_rolaDado(void);
 
 
 
-PAR_CondRet PAR_inicia(int numJog,JOG_tpCor *vetCores)
+PAR_CondRet PAR_inicia(JOG_tpCor *vetCores)
 {
     int i;
+    int numJog = PAR_obterNumJogadores();
     if(numJog>NUM_MAX_JOGADORES || numJog<NUM_MIN_JOGADORES) return PAR_CondRetParametro;
 
     for(i=0;i<numJog;i++)
@@ -57,3 +58,13 @@ int PAR_rolaDado()
 {
     return rand()%(MAX_DADO-MIN_DADO+1)+MIN_DADO; /*Equivale à um número de MIN_DADO até MAX_DADO*/
 }
+
+static int PAR_obterNumJogadores() {
+    int numJogad;
+    do {
+        printf("Digite o numero de jogadores (de 2 a 4): ");
+        scanf("%d", &numJogad);
+    } while (numJogad < 2 || numJogad > 4);
+    return numJogad;
+}
+    
