4ae8fa836e95559b790ee7a4c25fff87af91a6ca
Thlamz <thlamenza@hotmail.com>
2019-06-17 11:11:35 -0300

    Avanco na função renomeada para executaRodada

--- Ludo/Partida.c
+++ Ludo/Partida.c
@@ -26,11 +26,11 @@
 #include <stdlib.h>
 
 /* Variáveis estáticas do módulo */
-static JOG_tpJogador vJogadores[NUM_MAX_JOGADORES];
+static vJogadores[NUM_MAX_JOGADORES];
 
 static int numeroDeJogadores = 0;
 
-/* Funções estáticas encapsuladas pelo módulo */
+/*Funções estátivas emcapsuladas pelo módulo*/
 static int PAR_rolaDado(void);
 
 
@@ -41,6 +41,8 @@ PAR_CondRet PAR_inicia(JOG_tpCor *vetCores)
     int i;
     int numJog = PAR_obterNumJogadores();
     if(numJog>NUM_MAX_JOGADORES || numJog<NUM_MIN_JOGADORES) return PAR_CondRetParametro;
+
+    numeroDeJogadores = numJog;
 
     for(i=0;i<numJog;i++)
     {
@@ -63,30 +65,79 @@ int PAR_rolaDado()
 static int PAR_obterNumJogadores() {
     int numJogad;
     do {
-        printf("Digite o numero de jogadores (de %d a %d): ", NUM_MIN_JOGADORES, NUM_MAX_JOGADORE);
+        printf("Digite o numero de jogadores (de 2 a 4): ");
         scanf("%d", &numJogad);
-    } while (numJogad < NUM_MIN_JOGADORES || numJogad > NUM_MAX_JODORES);
+    } while (numJogad < 2 || numJogad > 4);
     return numJogad;
 }
 
-static JOG_tpPeca * PAR_escolhePeca(JOG_tpJogador * jog) {
-    int totalPec = JOG_NumPecas(jog);
+static int PAR_escolhePeca(JOG_tpJogador * jog) {
+    int totalPec;
     int pecaEscolhida;
-    if (totalPec < 1) return NULL;
+    void * casas = JOG_ObterPosicoesDasPecas(jog, &totalPec);
+    if (totalPec < 1) exit(10);
     if (totalPec == 1) {
-        printf("Somente uma peça poderia ser escolhida, jogada feita automaticamente.")
+        printf("Somente uma peça poderia ser escolhida, jogada feita automaticamente.");
         return 1;
     }
     do {
         printf("Escolha a peça a ser jogada (entre 1 e %d): ", totalPec-1);
         scanf("%d", &pecaEscolhida);
     } while (pecaEscolhida < 1 || pecaEscolhida >= totalPec);
-    return JOG_ObterPecaNaPosicao(jog, pecaEscolhida);
+    return pecaEscolhida;
+}
+
+PAR_CondRet PAR_FazRodada(int turno)
+{
+    int indPeca,dado;
+
+    /*Vetor das escolhas feitas até o momento
+    * No inicio é zerado pois nenhuma escolha foi feita
+    * Se todas foram feitas não há jogada possível*/
+    int vEscolhas[4] = {0,0,0,0}; 
+
+    JOG_tpPeca *peca;
+    TAB_CondRet condRetTab;
+    char *cores[4] = {"Vermelho","Azul","Verde","Amarelo"};
+    printf("Jogador %s, sua vez.\n",cores[turno]);
+
+    dado = PAR_rolaDado();
+    printf("Voce rodou %d no dado.\n",dado);
+
+
+    do
+    {
+        indPeca = PAR_escolhePeca(vJogadores[turno]);
+        vEscolhas[indPeca] = 1;
+
+        peca = JOG_ObterPecaNaPosicao(vJogadores[turno],indPeca);
+        if(peca==NULL) return PAR_CondRetParametro; 
+        /*Retorna CondRetParametro quando a peça não é encontrada*/
+
+        if(condRetTab == TAB_CondRetChegouFinal)
+        {
+            /*Condição quando a peça chega no final*/
+            return PAR_CondRetOk;
+        }
+
+        if(condRetTab == TAB_CondRetNaoAndou)
+        {
+            if(vEscolhas[0] && vEscolhas[1] && vEscolhas[2] && vEscolhas[3])
+            {
+                printf("Nao ha jogadas possiveis, passando turno...\n");
+                break;
+            }
+            printf("Essa peca nao pode se mover, escolha outra.\n");
+        }
+    }while(condRetTab = TAB_FazJogada(peca,dado) != TAB_CondRetOk);
+    
+    printf("Fim da jogada.\n");
+    return PAR_CondRetOk;
 }
 
 void PAR_finaliza() {
     int i;
-    TAB_FinalizaTabuleiro()
+    TAB_FinalizaTabuleiro();
     for (i = 0; i < numeroDeJogadores; i++) {
         JOG_Deleta(vJogadores[i]);
     }
